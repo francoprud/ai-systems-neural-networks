f1261a95de334ecf3c80d246d024af92da801aac
aducret <aslducret@gmail.com>
2016-05-15 20:34:16 -0300

    Added get training set

--- src/simple-perceptron/test_network.m
+++ src/simple-perceptron/test_network.m
@@ -1,13 +1,12 @@
 % func: function to test
+% n: amount of bits
 % minimumError: minimum error that determines when to stop training
 % learingRate: positive parameter for gradient descent to work
 % activation_func: activation function (logistic function/transference function)
 % activation_eval: evaluate the activation function result
-function output = test_network(func, minimumError, learningRate, activation_func, activation_eval)
-  inputs = [0 0 0; 0 0 1; 0 1 0; 0 1 1; 1 0 0; 1 0 1; 1 1 0; 1 1 1];
-  outputs = func(inputs);
-  trainingSet{1} = inputs;
-  trainingSet{2} = outputs;
+function output = test_network(func, n, minimumError, learningRate, activation_func, activation_eval)
+  trainingSet = get_training_set(func, n);
+  inputs = trainingSet{1};
 
   networkWeights = neural_network(trainingSet, minimumError, learningRate, activation_func);
   randInput = inputs(randi(rows(inputs)), :)
