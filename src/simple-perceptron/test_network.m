9e8f1d00fc82c2555b6b03d59fe12590071581cf
aducret <aslducret@gmail.com>
2016-05-15 21:50:18 -0300

    Getting all results

--- src/simple-perceptron/test_network.m
+++ src/simple-perceptron/test_network.m
@@ -9,11 +9,15 @@ function output = test_network(func, n, minimumError, learningRate, activation_f
   inputs = trainingSet{1};
 
   networkWeights = neural_network(trainingSet, minimumError, learningRate, activation_func);
-  randInput = inputs(randi(rows(inputs)), :)
-  output = evaluate_network(networkWeights, randInput, activation_func, activation_eval);
+  
+  %randInput = inputs(randi(rows(inputs)), :)
+  %output = evaluate_network(networkWeights, randInput, activation_func, activation_eval);
 
-  % for i = 1:rows(inputs)
-  %   inp = inputs(i, :)
-  %   output = evaluate_network(networkWeights, inputs(i, :), activation_func, activation_eval)
-  % end
+  output{1} = inputs(1, :);
+  output{2} = evaluate_network(networkWeights, inputs(1, :), activation_func, activation_eval);
+  for i = 2:rows(inputs)
+    output{1} = [output{1}; inputs(i, :)];
+    output{2}(i) = evaluate_network(networkWeights, inputs(i, :), activation_func, activation_eval);
+  end
+  output{2} = output{2}';
 end
